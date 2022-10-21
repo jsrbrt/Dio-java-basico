c6e13f6dbc69ceb21c3934abb27ac00fb764e831
jsrbrt <robmai.fds@gmail.com>
2022-10-21 18:18:52 -0300

    atualização de conteúdo

---
+++ java-anatomia-classes/src/edu/roberto/anatomiaJava/SistemaDeNotas.java
@@ -0,0 +1,13 @@
+package edu.roberto.anatomiaJava;
+public class SistemaDeNotas {
+    public static void main (String [] args){
+        double mediaFinal = .5;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO" + mediaFinal);
+        else if (mediaFinal == 6)
+            System.out.println("RECUPERAÇÃO");
+        else 
+            System.out.println("APROVADO");
+    }
+    
+}
---
+++ java-anatomia-classes/src/edu/roberto/tiposVariaveis/AlgumaCoisa.java
@@ -0,0 +1,5 @@
+package edu.roberto.tiposVariaveis;
+
+public class AlgumaCoisa {
+    
+}
