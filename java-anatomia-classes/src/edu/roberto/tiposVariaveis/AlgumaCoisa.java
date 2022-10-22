cc4fe29f7f0ec423088858ba6e7c875fada532d9
jsrbrt <robmai.fds@gmail.com>
2022-10-21 23:20:52 -0300

    Aula de tipos e variaveis terminada

--- java-anatomia-classes/src/edu/roberto/tiposVariaveis/AlgumaCoisa.java
+++ java-anatomia-classes/src/edu/roberto/tiposVariaveis/AlgumaCoisa.java
@@ -1,5 +1,24 @@
 package edu.roberto.tiposVariaveis;
 
 public class AlgumaCoisa {
+    public static void main(String[] args) {
+        
+        long cpf = 987123456787885469l;
     
+        final int NUMERO = 0;
+        //int numero = 0;
+        //numero = 5;
+        //numero = 0;
+        //numero = 2;
+        int soma = 785412365;
+        int resultado =  NUMERO + soma;
+    
+        if (NUMERO==0)
+            System.out.print(resultado);
+        else
+            System.out.print("maior que zero");
+
+
+
+    }
 }
