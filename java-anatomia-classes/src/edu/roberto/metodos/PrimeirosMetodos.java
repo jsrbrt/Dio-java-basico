54283d6117f38b4e97b1a95dfac264cce36253ab
jsrbrt <robmai.fds@gmail.com>
2022-10-22 23:09:10 -0300

    Smart tv terminada e resolvido os problemas

--- java-anatomia-classes/src/edu/roberto/metodos/PrimeirosMetodos.java
+++ java-anatomia-classes/src/edu/roberto/metodos/PrimeirosMetodos.java
@@ -1,5 +1,20 @@
 package edu.roberto.metodos;
 
 public class PrimeirosMetodos {
-    
+    public static void main(String[] args) {
+        int dividendo = 25;
+        int divisor = 5;
+        double dividir = dividir(dividendo, divisor);
+            System.out.println(dividir);
+            
+    }
+    public static double dividir(int dividendo, int divisor){
+    return dividendo / divisor;
+    }
+    private void metodoPrivado(){
+
+    }
+    public static void gravar (){
+    ;
+    }
 }
