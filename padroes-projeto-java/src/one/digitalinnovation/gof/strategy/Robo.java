2e9f763b753ad97602b5c3c1b89dedf83bbb3116
jsrbrt <robmail.fds@gmail.com>
2022-11-18 13:34:21 -0300

    desafio de projeto spring

---
+++ padroes-projeto-java/src/one/digitalinnovation/gof/strategy/Robo.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.gof.strategy;
+
+public class Robo {
+    private Comportamento comportamento;
+
+    public void setComportamento(Comportamento comportamento) {
+        this.comportamento = comportamento;
+    }
+
+    public Comportamento getComportamento() {
+        return comportamento;
+    }
+
+    public void mover() {
+        comportamento.mover();
+    }
+
+    @Override
+    public String toString() {
+        return "Robo{" +
+                "comportamento=" + comportamento +
+                '}';
+    }
+    /*@Override
+    public String toString() {
+        return "Robô\n" +
+                "Comportamento = " + comportamento;
+    }*/
+}
